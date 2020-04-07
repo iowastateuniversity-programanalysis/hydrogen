995b3fd18fa4754c8e99f5d0c4b23fd7e4bc6e26
Ben Steenhoek <benjis@iastate.edu>
2020-04-07 22:19:49 +0000

    Improve build script

--- SystemBuilder.py
+++ SystemBuilder.py
@@ -3,50 +3,55 @@
 #           Compile Hydrogen           #
 ########################################
 import os
-print("Compiling Hydrogen")
-os.chdir("./build")
-os.system("ninja")
-if (os.path.isfile("./Result.txt")):
-    os.system("rm ./Result.txt")
-if (os.path.isfile("./MVICFG.dot")):
-    os.system("rm ./MVICFG.dot")
-os.chdir("../")
+print('Compiling Hydrogen')
+os.chdir('./build')
+os.system('ninja')
+if (os.path.isfile('./Result.txt')):
+    os.system('rm ./Result.txt')
+if (os.path.isfile('./MVICFG.dot')):
+    os.system('rm ./MVICFG.dot')
+os.chdir('../')
 ########################################
 #           Path Settings              #
 ########################################
-demarcator = ":: "
-pathToDir = "./TestPrograms/"
-pathToModule1 = pathToDir + "Buggy/Prog.bc "
-pathToModule2 = pathToDir + "Buggy2/Prog.bc "
-pathToModule3 = pathToDir + "Correct/Prog.bc "
-filesModule1 = ["Buggy/Prog.c"]
-filesModule2 = ["Buggy2/Prog.c"]
-filesModule3 = ["Correct/Prog.c"]
-pathToFilesModule1 = ""
-pathToFilesModule2 = ""
-pathToFilesModule3 = ""
-for file in filesModule1:
-    pathToFilesModule1 = pathToFilesModule1 + os.path.abspath(pathToDir) + "/" + file + " "
-for file in filesModule2:
-    pathToFilesModule2 = pathToFilesModule2 + os.path.abspath(pathToDir) + "/" + file + " "
-for file in filesModule3:
-    pathToFilesModule3 = pathToFilesModule3 + os.path.abspath(pathToDir) + "/" + file + " "
-pathAbs = os.path.abspath(pathToModule1) + os.path.abspath(pathToModule2) + os.path.abspath(
-    pathToModule3) + demarcator + os.path.abspath(pathToFilesModule1) + demarcator + os.path.abspath(
-        pathToFilesModule2) + demarcator + os.path.abspath(pathToFilesModule3)
-print("Argument:\n" + pathAbs)
+demarcator = ' :: '
+pathToDir = os.path.abspath('./TestPrograms/')
+
+files = {
+    'Buggy': {
+        'Source': ['Prog.c'],
+        'Bytecode': 'Prog.bc',
+    },
+    'Buggy2': {
+        'Source': ['Prog.c'],
+        'Bytecode': 'Prog.bc',
+    },
+    'Correct': {
+        'Source': ['Prog.c'],
+        'Bytecode': 'Prog.bc',
+    },
+}
+
+input_files_relative = [[os.path.join(k, f) for f in v['Source']] for k, v in files.items()]
+input_files = [[os.path.join(pathToDir, f) for f in fl] for fl in input_files_relative]
+
+output_files_relative = [os.path.join(k, v['Bytecode']) for k, v in files.items()]
+output_files = [os.path.join(pathToDir, f) for f in output_files_relative]
+
+path_argument = demarcator.join([
+    ' '.join(output_files),
+    *(' '.join(fl) for fl in input_files)
+])
+print('Argument:\n' + path_argument)
 ########################################
 #         Running Hydrogen             #
 ########################################
-print("Running Hydrogen")
-os.chdir("./build")
-hydrogenCall = "./Hydrogen.out " + pathAbs
-os.system(hydrogenCall)
-os.chdir("../")
+print('Running Hydrogen')
+os.chdir('./build')
+os.system(f'./Hydrogen.out {path_argument}')
+os.chdir('../')
 ########################################
 #        Collecting Results            #
 ########################################
-resultMove = "cp ./build/Result.txt " + pathToDir + "Result.txt"
-os.system(resultMove)
-resultMove = "cp ./build/MVICFG.dot " + pathToDir + "MVICFG.dot"
-os.system(resultMove)
+os.system(f'mv ./build/Result.txt {pathToDir}/Result.txt')
+os.system(f'mv ./build/MVICFG.dot {pathToDir}/MVICFG.dot')
