4053cb03629ea1e52dfdaceb7d659ef73869caec
Ben Steenhoek <benjis@iastate.edu>
2020-04-11 01:30:41 +0000

    Change Prog.c back

--- TestPrograms/Buggy/Prog.c
+++ TestPrograms/Buggy/Prog.c
@@ -3,9 +3,8 @@ int main() {
   float a, b, area;
   scanf("%f %f", &a, &b);
   area = .5 * a * b;
-  if (area > 0) {
+  if (area > 0)
     printf("The area of (%.4f,%.4f),(%.4f,0) and (0,%.4f) is %.4f.", a, b, a, b, area);
-  }
   else
     printf("The area of (%.4f,%.4f),(%.4f,0) and (0,%.4f) is %.4f.", a, b, a, b, -area);
   return 0;
