59704c2ab1cf7f51400e34a80236e53e1bb9fb9c
bstee615 <benjaminjsteenhoek@gmail.com>
2020-04-17 22:06:03 +0000

    Add example usage

--- hydrogit/arguments.py
+++ hydrogit/arguments.py
@@ -1,7 +1,20 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 def get_args():
-    parser = ArgumentParser(prog='python hydrogit.py')
+    example_usage = '''example:
+    python hydrogit.py \\
+        -l C \\
+        https://github.com/gydrogen/progolone.git \\
+        5e8651df381079d0347ddfa254f554972611d1a0 \\
+        70d03532975252bd9982beba60a8720e11ec8f02 \\
+        9cde7197d0a3fe0caf7ee0ec7fd291e19ccc18ed
+'''
+
+    parser = ArgumentParser(
+        prog='python hydrogit.py',
+        epilog=example_usage,
+        formatter_class=RawDescriptionHelpFormatter
+    )
 
     parser.add_argument(
         '-p',
